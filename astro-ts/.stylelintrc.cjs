525237f9297feef8f8e71d836ad9a8e12982a714
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-07-08 16:55:49 +0900

    update astro config

--- astro-ts/.stylelintrc.cjs
+++ astro-ts/.stylelintrc.cjs
@@ -1,7 +1,18 @@
 module.exports = {
-  customSyntax: 'postcss-html',
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
   rules: {
     'order/properties-alphabetical-order': null,
   },
+  overrides: [
+    {
+      files: ['*.astro', '**/*.astro'],
+
+      // `*.astro` では postcss-html を指定しないとシンタックスエラーになってしまう
+      customSyntax: 'postcss-html',
+    },
+    {
+      files: ['*.{js,jsx,ts,tsx}', '**/*.{js,jsx,ts,tsx}'],
+      customSyntax: 'postcss-styled-syntax',
+    },
+  ],
 }
